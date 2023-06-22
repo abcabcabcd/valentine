b9e4b8a0d4af5373144d9cfbe058229da8fd059e
Valentine <33168264+owoValentine@users.noreply.github.com>
2023-06-22 13:05:22 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const images = document.querySelectorAll('.fade-in');
+
+  function fadeInImages() {
+    images.forEach(function(image) {
+      if (isElementInViewport(image) && !image.classList.contains('show')) {
+        image.classList.add('show');
+      }
+    });
+  }
+
+  function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  window.addEventListener('scroll', fadeInImages);
+  window.addEventListener('resize', fadeInImages);
+
+  // Call it
